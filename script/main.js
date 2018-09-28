d16f525c2b052e3fbd849695222f05fd221c2f60
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-09-28 12:07:33 +0100

    Read and display JSON file

--- script/main.js
+++ script/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-  $.ajax({
-    dataType: "json",
-    url: "../data.json",
-    success: function( data ) {
-      $('#json-renderer').jsonViewer(data);
-    },
-  });
+  function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+  }
+  jsonObj = JSON.parse(Get("data.json"));
+  // console.log(jsonObj);
+  $('#json-renderer').jsonViewer(jsonObj);
 });
