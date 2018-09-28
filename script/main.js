8e2cfc4ee24153f8e7b1c9d044c516b2b19c0dba
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-09-28 12:40:29 +0100

    Enable stock hiding

--- script/main.js
+++ script/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  $('input[name=hide-stock]').change(function(){
+    if($(this).is(':checked')) {
+        $('a:contains("stock")').parent().addClass('hidden');
+        $("li").filter(function(){return $(this).text() === "stock: []"}).addClass('hidden');
+    } else {
+      $('a:contains("stock")').parent().removeClass('hidden');
+      $("li").filter(function(){return $(this).text() === "stock: []"}).removeClass('hidden');
+    }
+  });
   function Get(yourUrl){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl,false);
